3c940bc0996458cdffd6148f27b56a9867ef1e8d
eresha1231 <eresha1231@gmail.com>
2020-10-02 23:20:06 +0700

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-
-exports.min = function min (array) {
-  return 0;
+const sortArr = (arr) => {  
+  return arr.slice().sort((a, b) => a - b)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.min = function min (array) {
+ return (Array.isArray(array) && array.length) ? sortArr(array)[0] : 0;
+}
+
+exports.max = function max (array) { 
+  return (Array.isArray(array) && array.length) ? sortArr(array)[array.length - 1] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (Array.isArray(array) && array.length) ? array.reduce((sum, current) => sum + current) / array.length : 0;
 }
